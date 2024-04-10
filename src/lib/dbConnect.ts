ac33ebe24911907399987a4d354eb5682364a31f
Supradeep <supradeep2004@gmail.com>
2024-04-11 03:03:31 +0530

    DB Connection Function

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,22 @@
+import { connect } from "mongoose"
+
+type ConnectionObject = { isConnected?: number }
+
+const connection: ConnectionObject = {}
+
+const dbConnect: Promise<void> = async () => {
+    if (connection.isConnected) {
+        console.log('Already connected to DB')
+        return
+    }
+    try {
+        const db = await connect(process.env.MONGO_URI)
+        connection.isConnected = db.connections[0].readyState
+        console.log('Connected to DB')
+    } catch (err) {
+        console.log('Connection to DB failed', err)
+        process.exit(1)
+    }
+}
+
+export default dbConnect
