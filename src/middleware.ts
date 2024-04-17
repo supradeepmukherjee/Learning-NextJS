edb3039013afbd26008a3f132305fbd79853c6fb
Supradeep <supradeep2004@gmail.com>
2024-04-17 13:41:48 +0530

    Middleware (if no token)

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ export { default } from 'next-auth/middleware'
 export async function middleware(request: NextRequest) {//request
     const token = await getToken({ req: request })
     const url = request.nextUrl
+    if (!token && url.pathname.startsWith('/dashboard')) return NextResponse.redirect(new URL('/sign-in', request.url))
     if (token && (
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/verify') ||
